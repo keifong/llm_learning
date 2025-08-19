c08f488497844d782be56a972f7c0433b379b9a5
Kei Fong <chiakeifong@gmail.com>
2025-08-19 16:55:23 +0800

    testing

--- MyApp.py
+++ MyApp.py
@@ -1,3 +1,31 @@
+# import streamlit as st
+
+# st.write("Hello world!")
+
+import pandas as pd
 import streamlit as st
 
-st.write("Hello world!")
+data_df = pd.DataFrame(
+    {
+        "sales": [
+            [0, 4, 26, 80, 100, 40],
+            [80, 20, 80, 35, 40, 100],
+            [10, 20, 80, 80, 70, 0],
+            [10, 100, 20, 100, 30, 100],
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.AreaChartColumn(
+            "Sales (last 6 months)",
+            width="medium",
+            help="The sales volume in the last 6 months",
+            y_min=0,
+            y_max=100,
+         ),
+    },
+    hide_index=True,
+)
